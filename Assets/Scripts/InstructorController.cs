72579cf1304ab9528a49f8940c30167934f817cb
Jared Stokes <stokesjd@gmail.com>
2019-08-07 15:05:59 -0700

    first commit

---
+++ Assets/Scripts/InstructorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructorController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public List<GameObject> steps;
+    public int currentStep = 0;
+
+    private void Start()
+    {
+        steps[currentStep].SetActive(true);
+
+        for(int i = 0; i < steps.Count; i++)
+        {
+            if (i != currentStep)
+            {
+                steps[i].SetActive(false);
+            }
+        }
+    }
+    public void Continue()
+    {
+        if(currentStep < steps.Count - 1)
+        {
+            steps[currentStep].SetActive(false);
+            currentStep++;
+            steps[currentStep].SetActive(true);
+        }
+        else
+        {
+            steps[currentStep].SetActive(false);
+            currentStep = 0;
+            steps[currentStep].SetActive(true);
+            menuCanvas.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
